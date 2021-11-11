749df29d0836476ea986bd906af2b7d350422827
Kristina Kacmarova <kkacmaro@uwo.ca>
2021-11-10 20:04:13 -0500

    css

--- application.py
+++ application.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template
 application = Flask(__name__)
 @application.route('/')
 
-# def hello_world():
-# 	return 'Hello World'
-
 def index():
     return render_template('index.html')
+    # return 'Hello World'
