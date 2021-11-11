bf892d8205d9cde5f6234b274650a7321d71350a
Kristina Kacmarova <kkacmaro@uwo.ca>
2021-11-10 19:12:19 -0500

    test

---
+++ application.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+application = Flask(__name__)
+@application.route('/')
+
+# def hello_world():
+# 	return 'Hello World'
+
+def index():
+    return render_template('index.html')
