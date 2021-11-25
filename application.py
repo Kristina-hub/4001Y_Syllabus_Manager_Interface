54751ca480bd15f11bf025f0eaa229bf57b3f440
Kristina Kacmarova <kkacmaro@uwo.ca>
2021-11-24 19:09:37 -0500

    fixed button

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 application = Flask(__name__)
-@application.route('/')
 
+@application.route('/')
 def index():
     return render_template('index.html')
- 	# return 'Hello World'
+
+@application.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 # export FLASK_APP="application.py"
 # flask run
