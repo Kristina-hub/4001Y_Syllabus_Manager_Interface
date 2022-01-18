e2fc32fbd0a0059dd143739e35b4711c8e91fd5d
Kristina Kacmarova <kkacmaro@uwo.ca>
2022-01-18 01:03:42 -0500

    edit

--- application.py
+++ application.py
@@ -9,13 +9,10 @@
 
 from flask import Flask, render_template
 from static.py.test import Test #from file.py import class
-import os
 
 app = Flask(__name__)
-# app._static_folder = os.path.abspath("static/")
 
-# @application.route('/')
-@app.route('/')
+@application.route('/')
 def index():
 	return Test.test_func()
 
@@ -26,17 +23,8 @@ def index():
 # @application.route('/upload', methods=['GET', 'POST'])
 # def upload():
 #     return render_template('upload.html')
-
-# if __name__ == "__main__":
-#     print("test")
-
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host = '0.0.0.0',port=5000) 
-    
     
 '''
 export FLASK_APP="application.py"
-export FLASK_APP="main.py"
 flask run
 '''
