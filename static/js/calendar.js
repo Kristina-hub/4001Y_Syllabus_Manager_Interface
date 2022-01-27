f9f260c29e2ce691ecac64f1539549a951bcba3b
Kristina Kacmarova <kkacmaro@uwo.ca>
2022-01-27 18:07:16 -0500

    edit

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -8,13 +8,34 @@
 // --------------------------------------- //
 
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+      let modalBtns = [...document.querySelectorAll(".button")];
+      modalBtns.forEach(function (btn) {
+        btn.onclick = function () {
+          let modal = btn.getAttribute("data-modal");
+          document.getElementById(modal).style.display = "block";
+        };
+      });
+      let closeBtns = [...document.querySelectorAll(".close")];
+      closeBtns.forEach(function (btn) {
+        btn.onclick = function () {
+          let modal = btn.closest(".modal");
+          modal.style.display = "none";
+        };
+      });
+      window.onclick = function (event) {
+        if (event.target.className === "modal") {
+          event.target.style.display = "none";
+        }
+      };
+
+// function openForm() {
+//   document.getElementById("myForm").style.display = "block";
+// }
+// 
+// function closeForm() {
+//   document.getElementById("myForm").style.display = "none";
+// }
 
 // $(document).ready(function() {
 // 
