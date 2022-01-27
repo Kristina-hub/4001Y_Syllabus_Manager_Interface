31b1a6d4279b2b2ae00c2ac688c7e31e95c8cd3d
Kristina Kacmarova <kkacmaro@uwo.ca>
2022-01-27 17:52:30 -0500

    edit

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -8,31 +8,38 @@
 // --------------------------------------- //
 
 
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-$(document).ready(function() {
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
-	function loadFile(form, filename) {    	
- 		const formData = new FormData(form); 		
- 		var oReq = new XMLHttpRequest(); 		
- 		oReq.open("POST", "calendar", true); 		
- 		oReq.send(formData);
- 		oReq.onload = function(oEvent) {
-    		document.getElementById("text").innerHTML = oReq.response;
-     	};
-	}
-	
-    $(".calendar-select").on('change', function() {
-    	document.getElementById('scroll').style.visibility = "hidden"; 
-    	document.getElementById("text2").innerHTML = "";
-    	document.getElementById("text").innerHTML = "Loading...";
-    	document.getElementById("middle").style.opacity = "0.5";
-//     	loadFile(this.form, this.files[0].name);
-    });
-    
-    $(".calendar-button").on('click', function() {
-    	$(".calendar-select").click();
-    });
-    
-});
+// $(document).ready(function() {
+// 
+// 	function loadFile(form, filename) {    	
+//  		const formData = new FormData(form); 		
+//  		var oReq = new XMLHttpRequest(); 		
+//  		oReq.open("POST", "calendar", true); 		
+//  		oReq.send(formData);
+//  		oReq.onload = function(oEvent) {
+//     		document.getElementById("text").innerHTML = oReq.response;
+//      	};
+// 	}
+// 	
+//     $(".calendar-select").on('change', function() {
+//     	document.getElementById('scroll').style.visibility = "hidden"; 
+//     	document.getElementById("text2").innerHTML = "";
+//     	document.getElementById("text").innerHTML = "Loading...";
+//     	document.getElementById("middle").style.opacity = "0.5";
+// //     	loadFile(this.form, this.files[0].name);
+//     });
+//     
+//     $(".calendar-button").on('click', function() {
+//     	$(".calendar-select").click();
+//     });
+//     
+// });
 
 
