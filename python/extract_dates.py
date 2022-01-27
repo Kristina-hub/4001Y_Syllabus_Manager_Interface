0e818c3c058ccf888fc869479cba03d5c59e126f
Kristina Kacmarova <kkacmaro@uwo.ca>
2022-01-27 15:03:45 -0500

    edit

--- python/extract_dates.py
+++ python/extract_dates.py
@@ -16,7 +16,7 @@ class ExtractDates():
 
 	def dates_func(text, filename):
 		print("Enter: deadlines_func.py")
-		csv_path = str(pathlib.Path().cwd()) + 'output/csv/extract_dates.csv'
+		csv_path = os.path.realpath(__file__) + 'output/csv/extract_dates.csv'
 		
 		'''Type: Assignment/Test/Project/Final/Midterm'''
 		df = pd.DataFrame(columns = ['File', 'Course', 'Deliverable', 'Date', 'Type'])  
