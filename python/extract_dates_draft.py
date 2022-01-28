783439e710dea6db85cdf66c9766b31f0ccf7c3b
Miranda <mpostma3@uwo.ca>
2022-01-28 12:15:43 -0500

    Started work to get course and date context

--- python/extract_dates_draft.py
+++ python/extract_dates_draft.py
@@ -19,11 +19,21 @@ class ExtractDates:
 		print("Enter: deadlines_func.py")
 
 		'''Type: Assignment/Test/Project/Final/Midterm'''
-		df = pd.DataFrame(columns = ['File', 'Course', 'Deliverable', 'Date', 'Type'])  
+		df = pd.DataFrame(columns = ['File', 'Course', 'Deliverable', 'Date'])
+		context = []
 		dates = []
+
+		result = re.search(r'[a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9]', text)
+		if result == None:
+			course = "Not Found"
+		else:
+			course = result.group()
 	
 		'''YYYY-MM-DD'''
-		dates += re.findall(r'\d{4}-\d{2}-\d{2}', text)
+		dates_list = re.findall(r'(\S+ \S+ )(\d{4}-\d{2}-\d{2})', text)
+		for x in dates_list:
+			context.append(x[0])
+			dates.append(x[1])
 	
 		'''Month DD, YYYY'''
 		'''Month D, YYYY'''
@@ -31,35 +41,47 @@ class ExtractDates:
 		'''Month DD-DD, YYYY'''
 		'''Month DD'''
 		'''Month D'''
-		dates_list = re.findall(r'((Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+(\d{1,2})(?:–\d{1,2})?(?:,)?(?:\s+(\d{4}))?)', text)
-		[dates.append(x[0]) for x in dates_list]
+		dates_list = re.findall(r'(\S+ \S+ )(((Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+(\d{1,2})(?:–\d{1,2})?(?:,)?(?:\s+(\d{4}))?))', text)
+		for x in dates_list:
+			context.append(x[0])
+			dates.append(x[1])
 
 		'''MM/DD'''
 		'''MM/D'''
 		'''M/D'''
 		'''M/DD'''
-		dates += re.findall(r'\d{1,2}\/\d{1,2}', text)
+		dates_list = re.findall(r'(\S+ \S+ )(\d{1,2}\/\d{1,2})', text)
+		for x in dates_list:
+			context.append(x[0])
+			dates.append(x[1])
 
 		'''MM/DD/YY'''
 		'''MM/DD/YYYY'''
 		'''DD/MM/YY'''
 		'''DD/MM/YYYY'''
-		dates += re.findall(r'\d{1,2}\/\d{1,2}\/(\d{4}|\d{2})', text)
-		
+		dates_list = re.findall(r'(\S+ \S+ )(\d{1,2}\/\d{1,2}\/(\d{4}|\d{2}))', text)
+		for x in dates_list:
+			context.append(x[0])
+			dates.append(x[1])
+
 		'''D-Month'''
 		'''DD-Month'''
 		'''D Month'''
 		'''DD Month'''
-		dates_list += re.findall(r'((\d{1,2})(-|\s)(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))', text)
-		[dates.append(x[0]) for x in dates_list]
+		dates_list = re.findall(r'(\S+ \S+ )((\d{1,2})(-|\s)(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))', text)
+		for x in dates_list:
+			context.append(x[0])
+			dates.append(x[1])
 
-		for date in dates:
+		for i in range(0, len(dates)):
+			date = dates[i]
+			con = context[i]
 			try:
 				parsed = parse(date, fuzzy_with_tokens=True)
 				date = parsed[0].strftime('%d-%B-%Y')
 			except:
 				print("Could not parse date: ", date)
-			row = {'File':filename, 'Course':'null', 'Deliverable':'null', 'Date':date, 'Type':'null'}
+			row = {'File':filename, 'Course':course, 'Deliverable':con, 'Date':date}
 			df = df.append(row, ignore_index=True)
 
 		print(dates)
