43142b0a77682495ccb80f1de9ca591ae0e6326a
Kristina Kacmarova <kkacmaro@uwo.ca>
2022-01-28 19:20:33 -0500

    fixed button functionality

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -8,58 +8,12 @@
 // --------------------------------------- //
 
 
+function openCalendar() {
+  document.getElementById('calendar').style.display='block';
+}
 
-// const modal = document.querySelector(".modal");
-// const trigger = document.querySelector(".trigger");
-// const closeButton = document.querySelector(".close-button");
-// 
-// function toggleModal() {
-//     modal.classList.toggle("show-modal");
-// }
-// 
-// function windowOnClick(event) {
-//     if (event.target === modal) {
-//         toggleModal();
-//     }
-// }
-// 
-// trigger.addEventListener("click", toggleModal);
-// closeButton.addEventListener("click", toggleModal);
-// window.addEventListener("click", windowOnClick);
-
-
-// function openForm() {
-//   document.getElementById("myForm").style.display = "block";
-// }
-// 
-// function closeForm() {
-//   document.getElementById("myForm").style.display = "none";
-// }
-
-// $(document).ready(function() {
-// 
-// 	function loadFile(form, filename) {    	
-//  		const formData = new FormData(form); 		
-//  		var oReq = new XMLHttpRequest(); 		
-//  		oReq.open("POST", "calendar", true); 		
-//  		oReq.send(formData);
-//  		oReq.onload = function(oEvent) {
-//     		document.getElementById("text").innerHTML = oReq.response;
-//      	};
-// 	}
-// 	
-//     $(".calendar-select").on('change', function() {
-//     	document.getElementById('scroll').style.visibility = "hidden"; 
-//     	document.getElementById("text2").innerHTML = "";
-//     	document.getElementById("text").innerHTML = "Loading...";
-//     	document.getElementById("middle").style.opacity = "0.5";
-// //     	loadFile(this.form, this.files[0].name);
-//     });
-//     
-//     $(".calendar-button").on('click', function() {
-//     	$(".calendar-select").click();
-//     });
-//     
-// });
+function closeCalendar() {
+  document.getElementById('calendar').style.display='none';
+}
 
 
--- static/js/download.js
+++ static/js/download.js
@@ -8,31 +8,12 @@
 // --------------------------------------- //
 
 
+function openDownload() {
+  document.getElementById('download').style.display='block';
+}
 
-$(document).ready(function() {
-
-	function loadFile(form, filename) {    	
- 		const formData = new FormData(form); 		
- 		var oReq = new XMLHttpRequest(); 		
- 		oReq.open("POST", "download", true); 		
- 		oReq.send(formData);
- 		oReq.onload = function(oEvent) {
-    		document.getElementById("text").innerHTML = oReq.response;
-     	};
-	}
-	
-    $(".download").on('change', function() {
-    	document.getElementById('scroll').style.visibility = "hidden"; 
-    	document.getElementById("text2").innerHTML = "";
-    	document.getElementById("text").innerHTML = "Loading...";
-    	document.getElementById("right").style.opacity = "0.5";
-    	loadFile(this.form, this.files[0].name);
-    });
-    
-    $(".download-button").on('click', function() {
-    	$(".download").click();
-    });
-    
-});
+function closeDownload() {
+  document.getElementById('download').style.display='none';
+}
 
 
