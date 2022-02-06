08d4960d97d3c82d89dba3c82d59d6e1a4f67c3a
Miranda <mpostma3@uwo.ca>
2022-02-06 17:56:04 -0500

    Worked on RegEx for Course/Context

--- python/extract_dates.py
+++ python/extract_dates.py
@@ -23,17 +23,58 @@ class ExtractDates:
 		context = []
 		dates = []
 
-		result = re.search(r'[a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9]', text)
+	# GET COURSE
+
+		# Check for Integrated Sci
+		result = re.search(r'Integrated Science [0-9][0-9][0-9][0-9][a-zA-Z]', text)
+
+		# Check for 4-letter course (i.e. CHEM1301A)
 		if result == None:
-			course = "Not Found"
-		else:
+			result = re.search(r'[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9][a-zA-Z]', text)
+
+		# Check for 4-letter course no letter (i.e. CHEM1301)
+		if result == None:
+			result = re.search(r'[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9]', text)
+
+		# Check for 3-letter course (i.e. BIO1000A)
+		if result == None:
+			result = re.search(r'[a-zA-Z][a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9][a-zA-Z]', text)
+
+		# Check for 3-letter course no letter (i.e. BIO1000)
+		if result == None:
+			result = re.search(r'[a-zA-Z][a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9]', text)
+
+		# Check for 2-letter course (i.e. CS1027A)
+		if result == None:
+			result = re.search(r'[a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9][a-zA-Z]', text)
+
+		# Check for 2-letter course no letter (i.e. CS1027)
+		if result == None:
+			result = re.search(r'[a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9]', text)
+
+		# Check for word + course (i.e. Biology 1000A)
+		if result == None:
+			result = re.search(r'\S+ [0-9][0-9][0-9][0-9][a-zA-Z]', text)
+
+		# Check for word + course no letter (i.e. Biology 1000)
+		# MAY GIVE FALSE POSITIVE! (May 2021, etc.)
+		if result == None:
+			result = re.search(r'\S+ [0-9][0-9][0-9][0-9]', text)
+
+		# Save result if available
+		if result != None:
 			course = result.group()
-	
+		else:
+			course = "Not Found"
+
+	# GET DATES
+
 		'''YYYY-MM-DD'''
-		dates_list = re.findall(r'(\S+ \S+ )(\d{4}-\d{2}-\d{2})', text)
+		dates_list = re.findall(r'(\w+)?[,:]?(\w+)?[,:]?(\d{4}-\d{2}-\d{2})', text)
+		# Exclude day names: ((?!Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday\b)\b\S+ )?
 		for x in dates_list:
-			context.append(x[0])
-			dates.append(x[1])
+			context.append(x[0] + ' ' + x[1])
+			dates.append(x[2])
 	
 		'''Month DD, YYYY'''
 		'''Month D, YYYY'''
@@ -41,37 +82,37 @@ class ExtractDates:
 		'''Month DD-DD, YYYY'''
 		'''Month DD'''
 		'''Month D'''
-		dates_list = re.findall(r'(\S+ \S+ )(((Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+(\d{1,2})(?:–\d{1,2})?(?:,)?(?:\s+(\d{4}))?))', text)
+		dates_list = re.findall(r'(\w+)?[,:]?(\w+)?[,:]?(((Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+(\d{1,2})(?:–\d{1,2})?(?:,)?(?:\s+(\d{4}))?))', text)
 		for x in dates_list:
-			context.append(x[0])
-			dates.append(x[1])
+			context.append(x[0] + ' ' + x[1])
+			dates.append(x[2])
 
 		'''MM/DD'''
 		'''MM/D'''
 		'''M/D'''
 		'''M/DD'''
-		dates_list = re.findall(r'(\S+ \S+ )(\d{1,2}\/\d{1,2})', text)
+		dates_list = re.findall(r'(\w+)?[,:]?(\w+)?[,:]?(\d{1,2}\/\d{1,2})', text)
 		for x in dates_list:
-			context.append(x[0])
-			dates.append(x[1])
+			context.append(x[0] + ' ' + x[1])
+			dates.append(x[2])
 
 		'''MM/DD/YY'''
 		'''MM/DD/YYYY'''
 		'''DD/MM/YY'''
 		'''DD/MM/YYYY'''
-		dates_list = re.findall(r'(\S+ \S+ )(\d{1,2}\/\d{1,2}\/(\d{4}|\d{2}))', text)
+		dates_list = re.findall(r'(\w+)?[,:]?(\w+)?[,:]?(\d{1,2}\/\d{1,2}\/(\d{4}|\d{2}))', text)
 		for x in dates_list:
-			context.append(x[0])
-			dates.append(x[1])
+			context.append(x[0] + ' ' + x[1])
+			dates.append(x[2])
 
 		'''D-Month'''
 		'''DD-Month'''
 		'''D Month'''
 		'''DD Month'''
-		dates_list = re.findall(r'(\S+ \S+ )((\d{1,2})(-|\s)(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))', text)
+		dates_list = re.findall(r'(\w+)?[,:]?(\w+)?[,:]?((\d{1,2})(-|\s)(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t)?(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))', text)
 		for x in dates_list:
-			context.append(x[0])
-			dates.append(x[1])
+			context.append(x[0] + ' ' + x[1])
+			dates.append(x[2])
 
 		for i in range(0, len(dates)):
 			date = dates[i]
